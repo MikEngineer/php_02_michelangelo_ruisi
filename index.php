43a4e49eac5f92da397601f080b798116faeafbf
Michelangelo Ruisi <mruisi96@gmail.com>
2025-01-14 13:44:36 +0100

    terminato selfwork 6

--- index.php
+++ index.php
@@ -68,5 +68,27 @@ if ($count > 0){
   echo "Non ci sono numeri pari nell'array." . "\n";
 };
 
+// Selfwork 6
+
+// Scrivere un programma che stampi in console tutti i numeri da uno a cento.
+// Se il numero è multiplo di 3 stampare “PHP” al posto del numero;
+// se multiplo di 5 stampare “JAVASCRIPT”;
+// se multiplo di 3 e 5 contemporaneamente deve stampare “HACKADEMY".
+
+
+$contatore = 0;
+
+for($i = 0; $i <= 100; $i++){
+    $contatore++;
+    if($contatore % 3 == 0 && $contatore % 5 == 0){
+      echo "HACKADEMY\n";
+    }elseif($contatore % 5 == 0){
+      echo "JAVASCRIPT\n";
+    }elseif($contatore % 3 == 0){
+      echo "PHP\n";
+    }else{
+      echo $contatore . "\n";
+    }
+  };
 
 ?>
