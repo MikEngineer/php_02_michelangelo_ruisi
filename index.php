be631a4ed0741c8d08166c1a57ffa823a58963ae
Michelangelo Ruisi <mruisi96@gmail.com>
2025-01-14 13:54:13 +0100

    inserito metodo usato nella soluzione

--- index.php
+++ index.php
@@ -91,4 +91,23 @@ for($i = 0; $i <= 100; $i++){
     }
   };
 
+  // Soluzione
+
+  // for($i = 1; $i <= 100; $i++){
+  //   switch($i){
+  //     case $i % 15 == 0:
+  //       echo "HACKADEMY \n";
+  //       break;
+  //     case $i % 5 == 0:
+  //       echo "JS \n";
+  //       break;
+  //     case $i % 3 == 0:
+  //       echo "PHP \n";
+  //       break;
+  //     default:
+  //       echo "$i \n";
+  //       break;
+  //   };
+  // };
+
 ?>
